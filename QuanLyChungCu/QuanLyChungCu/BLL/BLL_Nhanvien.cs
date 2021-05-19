515bf03e06921761b1398882d091b4e4dac700af
Kim Ngoc <hoangkimngoc44@gmail.com>
2021-05-19 23:28:37 +0700

    up up

---
+++ QuanLyChungCu/QuanLyChungCu/BLL/BLL_Nhanvien.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyChungCu.BLL
+{
+    class BLL_Nhanvien
+    {
+        private static BLL_Nhanvien _Instance;
+        public static BLL_Nhanvien Instance
+        {
+            get
+            {
+                if (_Instance == null) _Instance = new BLL_Nhanvien();
+                return _Instance;
+            }
+            private set { }
+        }
+        public IEnumerable<object> LayDSNV()
+        {
+            qlchungcuEntities db = new qlchungcuEntities();
+            var l1 = from nv in db.nhanviens
+                     join tk in db.taikhoans
+                     on nv.manhanvien equals tk.manhanvien
+                     select new
+                     {
+                         nv.manhanvien,
+                         nv.tennhanvien,
+                         nv.gioitinh,
+                         nv.sdt,
+                         nv.ngaysinh,
+                         nv.cmnd,
+                         nv.chucvu.tenchucvu,
+                         tk.tendangnhap,
+                         tk.matkhau
+                     };
+            return l1.ToList();
+        }
+        public bool Add_NV(nhanvien nv)
+        {
+            try
+            {
+                qlchungcuEntities db = new qlchungcuEntities();
+                var query = db.nhanviens.Where(p => p.manhanvien == nv.manhanvien).Count();
+                if (query == 0)
+                {
+                    db.nhanviens.Add(nv);
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
+        }
+        public bool Edit_NV(nhanvien nv)
+        {
+            try
+            {
+                qlchungcuEntities db = new qlchungcuEntities();
+                var query = (from p in db.nhanviens
+                             where p.manhanvien == nv.manhanvien
+                             select p).SingleOrDefault();
+                query.manhanvien = nv.manhanvien;
+                query.tennhanvien = nv.tennhanvien;
+                query.ngaysinh = nv.ngaysinh;
+                query.gioitinh = nv.gioitinh;
+                query.sdt = nv.sdt;
+                query.cmnd = nv.cmnd;
+                query.machucvu = nv.machucvu;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
+        }
+        
+        public void Delete_NV(int manv)
+        {
+            qlchungcuEntities db = new qlchungcuEntities();
+            var nv = db.nhanviens.Where(p => p.manhanvien == manv).SingleOrDefault();
+            db.nhanviens.Remove(nv);
+            db.SaveChanges();
+        }
+        public IEnumerable<object> GetListNVByTen(string tennv)
+        {
+            qlchungcuEntities db = new qlchungcuEntities();
+            var l = from nv in  db.nhanviens
+                    join tk in db.taikhoans
+                    on nv.manhanvien equals tk.manhanvien
+                    where nv.tennhanvien.ToLower().Contains(tennv.ToLower())
+                    select new
+                    {
+                        nv.manhanvien,
+                        nv.tennhanvien,
+                        nv.gioitinh,
+                        nv.sdt,
+                        nv.ngaysinh,
+                        nv.cmnd,
+                        nv.chucvu.tenchucvu,
+                        tk.tendangnhap,
+                        tk.matkhau
+                    };
+            return l.ToList();
+        }
+    }
+}
---
+++ QuanLyChungCu/QuanLyChungCu/BLL/BLL_TaiKhoan.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+
+namespace QuanLyChungCu.BLL
+{
+    class BLL_TaiKhoan
+    {
+        private static BLL_TaiKhoan _Instance;
+        public static BLL_TaiKhoan Instance
+        {
+            get
+            {
+                if (_Instance == null) _Instance = new BLL_TaiKhoan();
+                return _Instance;
+            }
+            private set { }
+        }
+        
+        
+        public bool Add_TK(taikhoan tk)
+        {
+            try
+            {
+                qlchungcuEntities db = new qlchungcuEntities();
+                var query = db.taikhoans.Where(p => p.tendangnhap == tk.tendangnhap).Count();
+                if (query == 0)
+                {
+                    db.taikhoans.Add(tk);
+                    db.SaveChanges();
+
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
+        }
+        public bool Edit_TK(taikhoan tk)
+        {
+            try
+            {
+                qlchungcuEntities db = new qlchungcuEntities();
+                var l = (from p in db.taikhoans
+                         where p.tendangnhap == tk.tendangnhap
+                         select p).SingleOrDefault();
+                l.tendangnhap = tk.tendangnhap;
+                l.matkhau = tk.matkhau;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
+        }
+
+        public void Delete_TK(int manv)
+        {
+            qlchungcuEntities db = new qlchungcuEntities();
+            var tk = db.taikhoans.Where(p => p.manhanvien == manv).SingleOrDefault();
+            db.taikhoans.Remove(tk);
+            db.SaveChanges();
+        }
+        
+    }
+}
